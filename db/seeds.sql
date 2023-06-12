d64ad21d2cd91983dfac94087e82dfeb2cd11c7a
Krystal Richardson <krystal.williams2019@gmail.com>
2023-06-12 15:56:02 -0400

    changes to roles and employee roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,19 +7,19 @@ VALUES  ('Engineering'),
         ('Human Resources');
 -- sample roles --
 INSERT INTO role (title, salary, department_id) 
-VALUES  ('Engineer', 50000, 1),
+VALUES  ('Engineer', 150000, 1),
         ('Advertisement Specialist', 70000, 2),
-        ('Salesperson', 20000, 3),
-        ('Sales Representative', 25000, 3)
-        ('Accountant I', 60000, 4),
-        ('Finance Assistant', 65000, 4)
-        ('HR Generalist', 35000, 5);
+        ('Salesperson', 100000, 3),
+        ('Sales Representative', 75000, 3)
+        ('Accountant I', 65000, 4),
+        ('Finance Assistant', 55000, 4)
+        ('HR Generalist', 62000, 5);
 -- sample employee names --
 INSERT INTO employee (first_name, last_name, role_id, manager_id) 
 VALUES  ('Kenny', 'Smith', 1, NULL),
-        ('Christina', 'Williams', 2, 1),
-        ('Luke', 'Johnson', 3, 2),
+        ('Christina', 'Williams', 2, NULL),
+        ('Luke', 'Johnson', 3, NULL),
         ('Mark', 'Lewis', 4, 3),
-        ('Crystal', 'Tyson', 5, 4),
-        ('Nicholas', 'Ingram', 6, 5)
-        ('Emily', 'Rouse', 7, 5);
+        ('Crystal', 'Tyson', 4, 3),
+        ('Nicholas', 'Ingram', 4, NULL)
+        ('Emily', 'Rouse', 5, NULL);
